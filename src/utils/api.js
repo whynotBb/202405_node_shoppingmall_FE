b1d8ab9444ac6c3265492cfbcd7442661c86feea
whynot-byoul <whynot.byoul@gmail.com>
2024-06-01 22:16:27 +0900

    로그인 기능 추가

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 // 상황따라 주소 다름
-// const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
+const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 // const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
 const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 
 const api = axios.create({
     baseURL: BACKEND_PROXY,
     headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${sessionStorage.getItem('token')}`,
+        "Content-Type": "application/json",
+        authorization: `Bearer ${sessionStorage.getItem("token")}`,
     },
 });
 /**
@@ -16,12 +16,14 @@ const api = axios.create({
  */
 api.interceptors.request.use(
     (request) => {
-        console.log('Starting Request', request);
-        request.headers.authorization = `Bearer ${sessionStorage.getItem('token')}`;
+        console.log("Starting Request", request);
+        request.headers.authorization = `Bearer ${sessionStorage.getItem(
+            "token"
+        )}`;
         return request;
     },
     function (error) {
-        console.log('REQUEST ERROR', error);
+        console.log("REQUEST ERROR", error);
     }
 );
 
@@ -31,7 +33,7 @@ api.interceptors.response.use(
     },
     function (error) {
         error = error.response.data;
-        console.log('RESPONSE ERROR', error);
+        console.log("RESPONSE ERROR", error);
         return Promise.reject(error);
     }
 );
