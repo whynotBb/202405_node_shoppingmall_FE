01ba25c34351a2f483eb281a160d095e9664b336
whynot-byoul <whynot.byoul@gmail.com>
2024-06-02 22:52:03 +0900

    토큰 로그인 / 로그아웃 기능 추가

--- src/action/userAction.js
+++ src/action/userAction.js
@@ -2,7 +2,26 @@ import api from "../utils/api";
 import * as types from "../constants/user.constants";
 import { commonUiActions } from "./commonUiAction";
 import * as commonTypes from "../constants/commonUI.constants";
-const loginWithToken = () => async (dispatch) => {};
+const loginWithToken = () => async (dispatch) => {
+    const token = sessionStorage.getItem("token");
+    console.log(token);
+    try {
+        dispatch({ type: types.LOGIN_WITH_TOKEN_REQUEST });
+        const response = await api.get("/user/me");
+        console.log("success", response);
+        if (response.status !== 200) {
+            throw new Error(response.Error);
+        }
+        console.log(response.data.user);
+        dispatch({
+            type: types.LOGIN_WITH_TOKEN_SUCESS,
+            payload: response.data.user,
+        });
+    } catch (error) {
+        dispatch({ type: types.LOGIN_WITH_TOKEN_FAIL });
+        dispatch(logout());
+    }
+};
 const loginWithEmail =
     ({ email, password }) =>
     async (dispatch) => {
@@ -18,7 +37,10 @@ const loginWithEmail =
             dispatch({ type: types.LOGIN_FAIL, payload: error.error });
         }
     };
-const logout = () => async (dispatch) => {};
+const logout = () => async (dispatch) => {
+    sessionStorage.removeItem("token");
+    dispatch({ type: types.LOGOUT });
+};
 
 const loginWithGoogle = (token) => async (dispatch) => {};
 
--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -10,12 +10,18 @@ function userReducer(state = initialState, action) {
     switch (type) {
         case types.REGISTER_USER_REQUEST:
         case types.LOGIN_REQUEST:
+        case types.LOGIN_WITH_TOKEN_REQUEST:
             return { ...state, loading: true };
         case types.LOGIN_SUCCESS:
-            return { ...state, loading: false, user: payload.user, error: "" };
+        case types.LOGIN_WITH_TOKEN_SUCESS:
+            return { ...state, loading: false, user: payload, error: "" };
         case types.LOGIN_FAIL:
         case types.REGISTER_USER_FAIL:
             return { ...state, loading: false, error: payload };
+        case type.LOGIN_WITH_TOKEN_FAIL:
+            return { ...state, loading: false };
+        case types.LOGOUT:
+            return { ...state, user: null };
         default:
             return state;
     }
