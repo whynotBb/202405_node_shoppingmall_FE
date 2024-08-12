7a182c15c9f4d15d30c1c8c33864a02c51d28c3b
whynot-byoul <whynot@hubilon.com>
2024-08-12 13:33:08 +0900

    FE배포

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,37 +5,35 @@ const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 
 const api = axios.create({
-    baseURL: LOCAL_BACKEND,
-    headers: {
-        "Content-Type": "application/json",
-        authorization: `Bearer ${sessionStorage.getItem("token")}`,
-    },
+	baseURL: BACKEND_PROXY,
+	headers: {
+		"Content-Type": "application/json",
+		authorization: `Bearer ${sessionStorage.getItem("token")}`,
+	},
 });
 /**
  * console.log all requests and responses
  */
 api.interceptors.request.use(
-    (request) => {
-        console.log("Starting Request", request);
-        request.headers.authorization = `Bearer ${sessionStorage.getItem(
-            "token"
-        )}`;
-        return request;
-    },
-    function (error) {
-        console.log("REQUEST ERROR", error);
-    }
+	(request) => {
+		console.log("Starting Request", request);
+		request.headers.authorization = `Bearer ${sessionStorage.getItem("token")}`;
+		return request;
+	},
+	function (error) {
+		console.log("REQUEST ERROR", error);
+	}
 );
 
 api.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    function (error) {
-        error = error.response.data;
-        console.log("RESPONSE ERROR", error);
-        return Promise.reject(error);
-    }
+	(response) => {
+		return response;
+	},
+	function (error) {
+		error = error.response.data;
+		console.log("RESPONSE ERROR", error);
+		return Promise.reject(error);
+	}
 );
 
 export default api;
