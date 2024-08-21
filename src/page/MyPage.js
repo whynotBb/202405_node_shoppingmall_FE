bc42a453e04774756d0795f08715cf85d5147e6a
whynot-byoul <whynot@hubilon.com>
2024-08-21 15:26:39 +0900

    마이페이지 수정

--- src/page/MyPage.js
+++ src/page/MyPage.js
@@ -18,7 +18,7 @@ const MyPage = () => {
 	}, []);
 	if (!loading) return <ColorRing visible={true} height="80" width="80" ariaLabel="blocks-loading" wrapperStyle={{}} wrapperClass="blocks-wrapper" colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]} />;
 	// 오더리스트가 없다면? 주문한 상품이 없습니다 메세지 보여주기
-	if (orderList.length === 0)
+	if (orderList === null)
 		return (
 			<Container>
 				<div>주문한 상품이 없습니다</div>
