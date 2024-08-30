d8081230874bca1b9a589d8fbd1730abc6f9f10c
whynot-byoul <whynot@hubilon.com>
2024-08-30 17:24:46 +0900

    주문한 상품이 없습니다. 예외처리

--- src/page/MyPage.js
+++ src/page/MyPage.js
@@ -16,18 +16,18 @@ const MyPage = () => {
 	useEffect(() => {
 		dispatch(orderActions.getOrderList());
 	}, []);
-	if (!loading) return <ColorRing visible={true} height="80" width="80" ariaLabel="blocks-loading" wrapperStyle={{}} wrapperClass="blocks-wrapper" colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]} />;
+	if (!loading || orderList === null) return <ColorRing visible={true} height="80" width="80" ariaLabel="blocks-loading" wrapperStyle={{}} wrapperClass="blocks-wrapper" colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]} />;
 	// 오더리스트가 없다면? 주문한 상품이 없습니다 메세지 보여주기
-	if (orderList === null)
-		return (
-			<Container>
-				<div>주문한 상품이 없습니다</div>
-			</Container>
-		);
+	// if (orderList === null)
+	// 	return (
+	// 		<Container>
+	// 			<div>주문한 상품이 없습니다</div>
+	// 		</Container>
+	// 	);
 
 	return (
 		<Container className="status-card-container">
-			{orderList.length === 0 ? (
+			{orderList?.length === 0 ? (
 				<Container>
 					<div>주문한 상품이 없습니다</div>
 				</Container>
